0f2fcb975177694253c5ca8cca2b6390a077c92e
tomekr <tomekr@gmail.com>
2019-11-06 15:25:58 +0100

    Add filter

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,20 @@
         <div class="col font-weight-bold">Zadanie</div>
         <div class="col-2 font-weight-bold">Zakończono ?</div>
       </div>
-      <div class="row" v-for="t in task" v-bind:key="t.action">
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
         <div class="col">{{t.action}}</div>
-        <div class="col-2">{{t.done}}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="t.done" class="form-check-input" />
+        </div>  
       </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-cnter">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-inpu" />
+          <label class="form-check-label font-weight-bold">
+            Ukryj zakończone zadania
+          </label>
+        </div>
+      </div>      
     </div>    
   </div>
 </template>
@@ -21,16 +31,22 @@
 
 export default {
   name: 'app',
-  data(){
-    return{
+  data() {
+    return {
       name: "Tomek",
-      task:[{action: "Kup kwiaty", done: false},
-            {action: "Zrób zakupy", done: false},
-            {action: "Wynieś Smieci", done: false}]
-      
+      tasks:[{action: "Kup kwiaty", done: false},
+            {action: "Zrób zakupy", done: true},
+            {action: "Wynieś Smieci", done: false},
+            {action: "Posprzątaj pokój", done: true}],
+      hideCompleted: true,
     }
-  }
-  
+  },
+ computed: {
+            filteredTasks() {
+                return this.hideCompleted ?
+                    this.tasks.filter(t => !t.done) : this.tasks
+             } 
+        },
 }
 </script>
 
