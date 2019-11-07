b9d0cd999c18caafca9da6c654c68a5ca098f0f6
tomekr <tomekr@gmail.com>
2019-11-07 12:07:48 +0100

    Add task store

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,23 @@
   <div id="app">
     <h4 class="bg-primary text-white text-center p-2">Lista zadań użytkownika {{name}}</h4>
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Zadanie</div>
-        <div class="col-2 font-weight-bold">Zakończono ?</div>
-      </div>
-      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-        <div class="col">{{t.action}}</div>
-        <div class="col-2 text-center">
-          <input type="checkbox" v-model="t.done" class="form-check-input" />
+      <div class="row" v-if="filteredTasks.lenght == 0">
+        <div class="col text-center">
+          <b>Nie masz nic do zrobienia</b>
         </div>
       </div>
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Zadanie</div>
+          <div class="col-2 font-weight-bold">Zakończono ?</div>
+        </div>
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+          <div class="col">{{t.action}}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="t.done" class="form-check-input" />
+          </div>
+        </div>
+      </template>
       <div class="row py-2">
         <div class="col">
           <input v-model="newItemText" class="form-control" />
@@ -24,6 +31,9 @@
         <div class="col text-center">
           <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
           <label class="form-check-label font-weight-bold">Ukryj zakończone zadania</label>
+        </div>
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">Usuń zakończone</button>
         </div>
       </div>
     </div>
@@ -53,7 +63,15 @@ export default {
         done: false
       });
       localStorage.setItem("todos", JSON.stringify(this.tasks));
+      this.storeData();
       this.newItemText = "";
+    },
+    storeData() {
+      localStorage.setItem("todos", JSON.stringify(this.tasks));
+    },
+    deleteCompleted() {
+      this.tasks = this.tasks.filter(t => !t.done);
+      this.storeData();
     }
   },
   created() {
