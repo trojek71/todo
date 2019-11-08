8822bf6a51755c787cc5e9a5944dcb9f0ed76e75
tomekr <tomekr@gmail.com>
2019-11-08 12:23:30 +0100

    Change background

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,41 @@
 <template>
-  <div id="app">
-    <h4 class="bg-primary text-white text-center p-2">Lista zadań użytkownika {{name}}</h4>
-    <div class="container-fluid p-4">
-      <div class="row" v-if="filteredTasks.lenght == 0">
-        <div class="col text-center">
-          <b>Nie masz nic do zrobienia</b>
-        </div>
-      </div>
-      <template v-else>
-        <div class="row">
-          <div class="col font-weight-bold">Zadanie</div>
-          <div class="col-2 font-weight-bold">Zakończono ?</div>
-        </div>
-        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-          <div class="col">{{t.action}}</div>
-          <div class="col-2 text-center">
-            <input type="checkbox" v-model="t.done" class="form-check-input" />
+  <div class="main">
+    <div id="app">
+      <h4 class="bg-primary text-white text-center p-2">Lista zadań użytkownika {{name}}</h4>
+      <div class="container-fluid p-4">
+        <div class="row" v-if="filteredTasks.lenght == 0">
+          <div class="col text-center">
+            <b>Nie masz nic do zrobienia</b>
           </div>
         </div>
-      </template>
-      <div class="row py-2">
-        <div class="col">
-          <input v-model="newItemText" class="form-control" />
+        <template v-else>
+          <div class="row">
+            <div class="col font-weight-bold">Zadanie</div>
+            <div class="col-2 font-weight-bold">Zakończono ?</div>
+          </div>
+          <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+            <div class="col">{{t.action}}</div>
+            <div class="col-2 text-center">
+              <input type="checkbox" v-model="t.done" class="form-check-input" />
+            </div>
+          </div>
+        </template>
+        <div class="row py-2">
+          <div class="col">
+            <input v-model="newItemText" class="form-control" />
+          </div>
+          <div class="col">
+            <button class="btn-primary" v-on:click="addNewTodo">Dodaj</button>
+          </div>
         </div>
-        <div class="col">
-          <button class="btn-primary" v-on:click="addNewTodo">Dodaj</button>
-        </div>
-      </div>
-      <div class="row bg-secondary py-2 mt-2 text-white">
-        <div class="col text-center">
-          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
-          <label class="form-check-label font-weight-bold">Ukryj zakończone zadania</label>
-        </div>
-        <div class="col text-center">
-          <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">Usuń zakończone</button>
+        <div class="row bg-secondary py-2 mt-2 text-white">
+          <div class="col text-center">
+            <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+            <label class="form-check-label font-weight-bold">Ukryj zakończone zadania</label>
+          </div>
+          <div class="col text-center">
+            <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">Usuń zakończone</button>
+          </div>
         </div>
       </div>
     </div>
@@ -83,4 +85,6 @@ export default {
 };
 </script>
 
-
+<style lang="scss" scoped>
+@import "./src/styles/global.scss";
+</style>
